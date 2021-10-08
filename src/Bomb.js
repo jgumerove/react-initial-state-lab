21ecb597642fd5c05791ef2a8b092860ccb66cf4
jgumerove <josh.gumerove@gmail.com>
2021-10-08 16:11:49 -0400

    commit tree 93b0d36b1f22f154a178f873bc9879a94d348c64

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,33 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             secondsLeft: this.props.initialCount,
+        }
+    }
+
+    decrease = () => {
+        this.setState({
+            secondsLeft: this.state.secondsLeft - 1
+        })
+    }
+    
+    render() {
+        const {secondsLeft} = this.state
+        return secondsLeft > 0 ? (
+            <div onClick={this.decrease}>
+               {secondsLeft} seconds left before I go boom!
+            </div>
+        ) : (
+            <div>
+                Boom!
+            </div>
+        )
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,23 @@
 // your ImageSlider code here!
+import React, { Component } from 'react'
+
+class ImageSlider extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             currentSlideIndex: 0,
+        }
+    }
+    
+    render() {
+        const {currentSlideIndex} = this.state
+        return (
+            <div>
+                I am on slide {currentSlideIndex}
+            </div>
+        )
+    }
+}
+
+export default ImageSlider
